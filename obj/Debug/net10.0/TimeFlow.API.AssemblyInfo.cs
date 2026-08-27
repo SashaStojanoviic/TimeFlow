a51649e273e2d0975311c7500c9c802390dfee3c
SashaStojanoviic <sasha_stojanovic@outlook.com>
2026-08-27 10:40:44 +0200

    First commit

--- obj/Debug/net10.0/TimeFlow.API.AssemblyInfo.cs
+++ obj/Debug/net10.0/TimeFlow.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TimeFlow.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c00dd018bf9434aa0987ca812120317664a087a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TimeFlow.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TimeFlow.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
